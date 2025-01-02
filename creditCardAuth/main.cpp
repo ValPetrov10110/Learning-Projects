1772cc644436b1c94bc88e1e965b969dcfb9b44d
Valentin Petrov <valpetrov.soccer@gmail.com>
2025-01-02 02:03:19 -0500

    Added the comments to the main.cpp file in order to help remember the formula and to help with my logic

--- creditCardAuth/main.cpp
+++ creditCardAuth/main.cpp
@@ -1,4 +1,16 @@
 #include <iostream>
+/* 
+    I will be using the Luhn Algorithm for this project
+   
+    1.) Starting from the rightmost digit, double the value of every second digit and add them together
+        (If doubling of a number results in a two digit i.e greater than 9, split them and treat them as individual numbers to add)
+        Ex.) 79927398713 -> 9 + 2 + 3 + 8 + 1 -> 18 + 4 + 6 + 16 + 2 -> (1 + 8) + 4 + 6 + (1 + 6) + 2 -> 28
+    
+    2.) After you have added the numbers together from step 1, add the numbers leftover together and add to the final answer
+        Ex.) 7 + 9 + 7 + 9 + 7 + 3 -> 42 -> 42 + 28 -> 70
+
+    3.) If you can mod 10 the sum of step 2, then the number is valid according to the Luhn formula
+*/
 
 using std::cout;
 using std::cin;
