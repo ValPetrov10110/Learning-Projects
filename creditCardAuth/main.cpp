651d0b62d4dff32de3eca4970534565273a5d70a
Valentin Petrov <valpetrov.soccer@gmail.com>
2025-01-02 20:14:25 -0500

    Project Completed.

--- creditCardAuth/main.cpp
+++ creditCardAuth/main.cpp
@@ -23,21 +23,52 @@ int sumEvenDigits(const std::string cardNumber);
 
 int main(){
 
+    std::string cardNumber;
+    int result = 0;
+
+    cout << "Enter Credit Card Number > ";
+    cin >> cardNumber;
+
+    result = sumEvenDigits(cardNumber) + sumOddDigits(cardNumber);
+
+    if(result % 10 == 0){
+        cout << "Valid Card\n";
+    }
+    else{
+        cout << "Invalid Card\n";
+    }
+
 
     return 0;
 }
 
-int getDigits(const int number){
+int getDigit(const int number){
 
-    return 0;
+    if(number > 9){
+        return number - 9;
+    }
+
+    return number;
 }
 
-int sumOdDigits(const std::string cardNumber){
+int sumOddDigits(const std::string cardNumber){
+    
+    int sum = 0;
 
-    return 0;
+    for(int i = cardNumber.size() - 1; i >= 0; i -= 2){
+        sum += cardNumber[i] - '0';
+    }
+
+    return sum;
 }
 
 int sumEvenDigits(const std::string cardNumber){
 
-    return 0;
+    int sum = 0;
+
+    for(int i = cardNumber.size() - 2; i >= 0; i-=2){
+        sum += getDigit((cardNumber[i] - '0') * 2);
+    }
+
+    return sum;
 }
