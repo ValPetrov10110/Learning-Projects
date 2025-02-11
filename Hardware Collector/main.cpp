84cb24d45897799a332f58bef62e9028cc06c527
Valentin Petrov <valpetrov.soccer@gmail.com>
2025-02-11 18:00:36 -0500

    Created a test popup window with the API

--- Hardware Collector/main.cpp
+++ Hardware Collector/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <windows.h> // This is what gives me accessx to the Windows API Function
 
 using std::cout;
 using std::cin;
 
 int main(){
-
+    MessageBox(NULL, TEXT("Hello, Windows API!"), TEXT("Message Box"), MB_OK);
     return 0;
 }
