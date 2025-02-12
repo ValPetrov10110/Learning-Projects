14ff93105a9a91db9146a97e2716aa25c565e69d
Valentin Petrov <valpetrov.soccer@gmail.com>
2025-02-11 23:57:43 -0500

    This is where I left off, still reading out the API

--- Hardware Collector/main.cpp
+++ Hardware Collector/main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
-#include <windows.h> // This is what gives me accessx to the Windows API Function
+#include <windows.h> // This is what gives me access to the Windows API Function
 
 using std::cout;
 using std::cin;
 
+/*
+       NOTES
+    -------------
+    - Application/Main Window is the window we are all familiar with (frame w/ title bar, min/max buttons, etc.)
+*/
+
 int main(){
 
-    MessageBox(NULL, TEXT("Hello, Windows API!"), TEXT("Message Box"), MB_OK);
+    MessageBox(NULL, TEXT("Greetings Human"), TEXT("Message Box"), MB_OK);
     
     return 0;
 }
